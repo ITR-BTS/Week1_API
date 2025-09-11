e37867c804a9d6cfad3095146cfa1e525fa91bc6
DucSang404 <ndsang404@gmail.com>
2025-09-11 17:26:24 +0700

    keep v

--- src/graphql/typedefs/patientTypedef.js
+++ src/graphql/typedefs/patientTypedef.js
@@ -10,49 +10,55 @@ const patientTypeDef = gql`
   }
 
   type Patient{
-    id: ID!
-    email: String!
-    phone: String!
-    gender: String!
-    dob: String!
-    physician: Physician!
-    addressInfo: AddressInfo!
-}
-  input PatientFilter {
-    physicianId: ID
-}
-  type PatientList     {
+      id: ID!
+      email: String!
+      phone: String!
+      gender: String!
+      dob: String!
+      physician: Physician!
+      addressInfo: AddressInfo!
+  }
+
+  type PatientList {
     data: [Patient]
+    pageSize: Int
     total: Int
     page: Int
     totalPages: Int
-}
+  }
+
   type Query {
-  patients(page: Int, limit: Int, filter: PatientFilter): PatientList
-  patientByEmail(email: String!): Patient
-  patientDetails(id: ID!): Patient
-}
+    patients(page: Int, limit: Int, action: PatientQueryAction): PatientList
+    patientByEmail(email: String!): [Patient]
+    patientDetails(id: ID!): Patient
+  }
 
-type Mutation {
-  createPatient(email: String!, phone: String!, gender: String!, dob: String!, physician: ID!, addressInfo: AddressInput!): Patient
-  updatePatient(id: ID!, input: UpdatePatientInput!): Patient
-  deletePatient(id: ID!): Boolean
-}
+  type Mutation {
+    createPatient(email: String!, phone: String!, gender: String!, dob: String!, physician: ID!, addressInfo: AddressInput!): Patient
+    updatePatient(id: ID!, input: UpdatePatientInput!): Patient
+    deletePatient(id: ID!): Boolean
+  }
 
-input UpdatePatientInput {
-  email: String
-  phone: String
-  dob: String
-  physician: ID
-  addressInfo: AddressInput
-}
+  input PatientQueryAction {
+    gender: String
+    sortEmail: String
+    sortPhyEmail: String
+  }
 
-input AddressInput {
-  address: String!
-  city: String!
-  state: String!
-  country: String!
-}
+  input UpdatePatientInput {
+    email: String
+    phone: String
+    dob: String
+    physician: ID
+    addressInfo: AddressInput
+  }
+
+  input AddressInput {
+    address: String!
+    city: String!
+    state: String!
+    country: String!
+  }
 `
 
 export default patientTypeDef
